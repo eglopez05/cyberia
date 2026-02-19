87066bb3230c1af2c9e1704461c5cdc08bb100f3
willianorellana830-cell <weorellana@sudamericano.edu.ec>
2026-02-18 20:58:38 -0500

    Update imagenes.page.spec.ts

--- src/app/pages/imagenes/imagenes.page.spec.ts
+++ src/app/pages/imagenes/imagenes.page.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ImagenesPage } from './imagenes1.page';
+import { ImagenesPage } from './imagenes.page';
 
 describe('ImagenesPage', () => {
   let component: ImagenesPage;
